0eb88d9a4e4399160f0aed75a006fc39af6be247
Gregory Johnson <gj@andrew.cmu.edu>
2019-12-27 14:19:53 -0800

    merge fixes

--- ml_snek/tests/conftest.py
+++ ml_snek/tests/conftest.py
@@ -20,10 +20,17 @@ from typing import Dict
 
 import pytest
 
+from ..datasets.jsnek_saved_games_dataset import JSnekDataset
+
 
 @pytest.fixture
 def data_dir() -> Path:
     return Path(__file__).parent / "data"
+
+
+@pytest.figure
+def dataset_jsnek() -> JSnekDataset:
+    return JSnekDataset()
 
 # Fixtures can simply be added as a parameter to the other test or fixture functions to expose them.
 # If we had multiple tests that wanted to use the contents of this file, we could simply add "loaded_example_values" as
