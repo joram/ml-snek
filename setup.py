6e353b0b83a7b1596622efaa044c4b77577bea5c
Gregory Johnson <gj@andrew.cmu.edu>
2019-12-27 12:15:31 -0800

    refactor

---
+++ setup.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+test_requirements = [
+    "codecov",
+    "flake8",
+    "black",
+    "pytest",
+    "pytest-cov",
+    "pytest-raises",
+]
+
+setup_requirements = [
+    "pytest-runner",
+]
+
+dev_requirements = [
+    "bumpversion>=0.5.3",
+    "coverage>=5.0a4",
+    "flake8>=3.7.7",
+    "ipython>=7.5.0",
+    "m2r>=0.2.1",
+    "pytest>=4.3.0",
+    "pytest-cov==2.6.1",
+    "pytest-raises>=0.10",
+    "pytest-runner>=4.4",
+    "Sphinx>=2.0.0b1",
+    "sphinx_rtd_theme>=0.1.2",
+    "tox>=3.5.2",
+    "twine>=1.13.0",
+    "wheel>=0.33.1",
+]
+
+interactive_requirements = [
+    "altair",
+    "jupyterlab",
+    "matplotlib",
+]
+
+requirements = []
+
+extra_requirements = {
+    "test": test_requirements,
+    "setup": setup_requirements,
+    "dev": dev_requirements,
+    "interactive": interactive_requirements,
+    "all": [
+        *requirements,
+        *test_requirements,
+        *setup_requirements,
+        *dev_requirements,
+        *interactive_requirements
+    ]
+}
+
+setup(
+    author="Gregory Johnson",
+    author_email="gregjohnso@gmail.com",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="Machine learning snake",
+    entry_points={
+        "console_scripts": [
+            "my_example=ml_snek.bin.my_example:main"
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="ml_snek",
+    name="ml_snek",
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*"]),
+    python_requires=">=3.6",
+    setup_requires=setup_requirements,
+    test_suite="ml_snek/tests",
+    tests_require=test_requirements,
+    extras_require=extra_requirements,
+    url="https://github.com/gregjohnso/ml_snek",
+    # Do not edit this string manually, always use bumpversion
+    # Details in CONTRIBUTING.rst
+    version="0.1.0",
+    zip_safe=False,
+)
