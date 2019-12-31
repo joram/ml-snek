b342fbf812c5e10835553f44208e9835c485ddc8
Gregory Johnson <gj@andrew.cmu.edu>
2019-12-30 23:04:59 -0800

    refactor, add train.py kwargs

--- ml_snek/bin/train.py
+++ ml_snek/bin/train.py
@@ -1,26 +1,89 @@
 #!/usr/bin/env python3
-from ..dataloaders.dataloader import Dataloader
-from ..datasets.jsnek_saved_games_dataset import JSnekDataset
-from ..models.random_forest_model import RandomForestModel
-from ..trainers.basic_trainer import BasicTrainer
+
+import argparse
+import json
 import random
+import torch
+
+from .. import utils
 
 
 def main():
-    dataset = JSnekDataset()
-    dataloader = Dataloader(dataset)
-    model = RandomForestModel()
-    trainer = BasicTrainer(model, dataloader)
-    trainer.train(epochs=10)
+    parser = argparse.ArgumentParser()
+
+    # this will get populated as necessary
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="ml_snek.models.random_forest_model.RandomForest",
+        help="model name",
+    )
+    parser.add_argument(
+        "--model_kwargs",
+        type=json.loads,
+        default='{"n_estimators": 100}',
+        help="kwargs for the model",
+    )
+
+    parser.add_argument(
+        "--dataset_name",
+        type=str,
+        default="ml_snek.datasets.jsnek_dataset.JSnekDataset",
+        help="dataset name",
+    )
+    parser.add_argument(
+        "--dataset_kwargs",
+        type=json.loads,
+        default='{"board_state_as_vector": 1}',
+        help="kwargs for the dataset",
+    )
+
+    parser.add_argument(
+        "--dataloader_kwargs",
+        type=json.loads,
+        default='{"batch_size": 1000000}',
+        help="kwargs for the dataset",
+    )
+
+    parser.add_argument(
+        "--trainer_name",
+        type=str,
+        default="ml_snek.trainers.sklearn_trainer.SKLearnTrainer",
+        help="dataset name",
+    )
+    parser.add_argument(
+        "--trainer_kwargs",
+        type=json.loads,
+        default='{"n_epochs": 1}',
+        help="kwargs for the trainer",
+    )
+
+    args = parser.parse_args()
+
+    model = utils.load_object(args.model_name, args.model_kwargs)
+
+    dataset = utils.load_object(args.dataset_name, args.dataset_kwargs)
+    dataloader = torch.utils.data.dataloader.DataLoader(
+        dataset, **args.dataloader_kwargs
+    )
+
+    args.trainer_kwargs["model"] = model
+    args.trainer_kwargs["dataloader"] = dataloader
+
+    trainer = utils.load_object(args.trainer_name, args.trainer_kwargs)
+
+    trainer.train()
+
+    dataset = dataloader.dataset
 
     # random spot check evaluation
     for i in range(0, 100):
-        index = random.randint(0, len(dataset)-1)
+        index = random.randint(0, len(dataset) - 1)
         input_values, output_value = dataloader[index]
         expected = [output_value]
         actual = model.model.predict([input_values])
         print(f"expected: {expected}\tactual: {actual}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- ml_snek/models/random_forest_model.py
+++ ml_snek/models/random_forest_model.py
@@ -2,10 +2,9 @@ from .base_model import BaseModel
 from sklearn.ensemble import RandomForestClassifier
 
 
-class RandomForestModel(BaseModel):
-
-    def __init__(self):
-        self.model = RandomForestClassifier(n_jobs=2, random_state=0)
+class RandomForest(BaseModel):
+    def __init__(self, **kwargs):
+        self.model = RandomForestClassifier(**kwargs)
 
     def train(self, input_values, expected_output):
         self.model.fit(input_values, expected_output)
