c2ecd69d565ef45fbaca34e05c9f48203aa9c58f
Gregory Johnson <gj@andrew.cmu.edu>
2019-12-30 18:08:49 -0800

    fixed dataloader stuff by removing cached stuff

--- ml_snek/tests/test_dataloaders.py
+++ ml_snek/tests/test_dataloaders.py
@@ -3,14 +3,17 @@ from torch.utils.data import DataLoader
 
 def test_flat_dataloader(dataset_jsnek):
 
-    dataloader = DataLoader(dataset_jsnek, batch_size=32)
+    batch_size = 32
+
+    dataloader = DataLoader(dataset_jsnek, batch_size=batch_size)
 
     first_item = dataloader.__iter__().__next__()
 
-    assert type(first_item) == tuple
+    assert type(first_item) == list
 
     assert len(first_item) == 2
 
-    assert len(first_item[0]) == 484
+    for i in range(len(first_item)):
 
-    assert type(first_item[1]) == int
+        assert len(first_item[i]) == batch_size
+
